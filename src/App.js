b679ca0cdf71905f3d5dcf3cf1cd8153bd71cb0a
HoangDuy09042001 <duyht194748@sis.hust.edu.vn>
2024-01-06 16:42:28 +0700

    ios gif

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ function App() {
   const [showMcVideo, setShowMcVideo] = useState(false);
   const audioRef = useRef(null);
   const [pause, setPause] = useState(false);
-  const [imgdefault, setImgDefault] = useState(true);
   const [scale, setScale] = useState(1)
   const [width, setWidth] = useState(1920)
   const [height, setHeight] = useState(1080)
   const [videoId, setVideoId] = useState(null);
   const [currentTime, setCurrentTime] = useState(0);
   const mcVideoRef = useRef(null);
-
+  const [introGif, setIntroGif] = useState(true)
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const idFromUrl = urlParams.get('videoId');
     const idWidth = urlParams.get('width');
+
     console.log(idFromUrl)
     if (idFromUrl) {
       setVideoId(idFromUrl);
@@ -49,12 +49,12 @@ function App() {
     return () => {
       document.removeEventListener('click', handleUserInteraction);
     };
-  }, [pause,  showMcVideo]);
+  }, [pause, showMcVideo]);
 
   useEffect(() => {
     setScale(width / 1920)
     setHeight(width / 1920 * 1080)
-  }, [imgdefault, width]);
+  }, [width]);
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentTime((prevTime) => prevTime + 1);
@@ -63,32 +63,33 @@ function App() {
     return () => clearInterval(interval);
   }, []);
 
-  // Kiểm tra điều kiện để hiển thị video
-  if (currentTime >= 6 && !showMcVideo) {
-    setShowMcVideo(true);
-  }
+  useEffect(() => {
+    if (currentTime === 6 && !showMcVideo) {
+      setShowMcVideo(true);
+      
+    }
+    if(currentTime === 21)setIntroGif(false)
+
+  }, [currentTime, showMcVideo])
 
   return (
     <div className="App" style={{ position: 'relative', width: 'fit-content', margin: 'auto' }}>
-      
-      {imgdefault ? <div onClick={() => {
-        setImgDefault(false)
-      }} className='image-default' style={{ width: width + 'px', height: height + 'px' }}></div> : <></>}
+     
       <audio ref={audioRef} src="background_music.mp3" />
-      <div className="container" style={{ display: imgdefault ? 'none' : 'block' }}>
-        <div className="background getaway-car" style={{ width: width + 'px', height: height + 'px' }}></div>
-
+      <div className="container" >
+        {/* <div>{currentTime} {introGif?'INTRO':'OUTTRO'}</div> */}
+        {introGif && <div className="getaway-car intro" style={{ width: width + 'px', height: height + 'px', backgroundImage: "url('background.gif')"}}></div>}
+        <div className="getaway-car next-intro" style={{ width: width + 'px', height: height + 'px', backgroundImage: "url('outro.gif')", visibility: introGif?'hidden':'visible' }} ></div>
         {showMcVideo && (
-          <div style={{ borderRadius: '10', overflow: 'hidden', width: 600 * scale + 'px', height: 600 * scale + 'px' }}>
-            <video
-              className="mc"
-              autoPlay
-              width={600 * scale}
-              style={{ top: scale * (560 - 600 / 2) + 'px', left: scale * (1350 - 600 / 2) + 'px' }}
-              ref={mcVideoRef}
-              src={videoId ? `https://work247.vn/dowload/video_new/new_${videoId}/video_${videoId}.mp4` : 'mc.mp4'}
-            ></video>
-          </div>
+          <video
+            className="mc"
+            autoPlay
+            width={600 * scale}
+            style={{ top: scale * (560 - 600 / 2) + 'px', left: scale * (1350 - 600 / 2) + 'px' }}
+            ref={mcVideoRef}
+            src={videoId ? `https://work247.vn/dowload/video_new/new_${videoId}/video_${videoId}.mp4` : 'mc.mp4'}
+          ></video>
+
         )}
       </div>
 
